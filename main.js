65a2ff10238549f80eb2a9df2aca1ec5862522a1
zigridar <safonenko.max@yandex.ru>
2020-06-12 12:06:35 +0500

    screenshot functions

--- main.js
+++ main.js
@@ -2,15 +2,42 @@
 
 const io = require('socket.io-client')
 
-const url = 'http://217.151.235.59:3000/'
+const screenshot = require('screenshot-desktop')
+const fs = require('fs')
 
-const socket = io.connect(url, {
-    reconnect: false
-})
+/** return an arrayBuffer of desktop screenshot **/
+function takeScreenShot() {
+    return new Promise((resolve, reject) => {
+        screenshot()
+            .then(imgBuffer => resolve(imgBuffer))
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
 
-socket.emit('offer')
+/** save local screenshot **/
+function saveScreenShot(bufferedData, path) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, bufferedData, err => {
+            if (err)
+                reject(err)
+            else
+                resolve()
+        })
+    })
+}
 
-socket.on('answer', (data) => {
-    console.log(data)
-})
+//todo
+// const url = 'todo'
+//
+// const socket = io.connect(url, {
+//     reconnect: false
+// })
+//
+// socket.emit('offer')
+//
+// socket.on('answer', (data) => {
+//     console.log(data)
+// })
 
