be0b074b5a653bc7dc4c01973cb792f3d7893321
zigridar <safonenko.max@yandex.ru>
2020-06-14 01:37:51 +0500

    question table

--- local_test_server/src/index.js
+++ local_test_server/src/index.js
@@ -26,13 +26,13 @@ const galleryOptions_old = {
 }
 
 /** page init **/
-$(document).ready(function() {
+$(document).ready(async () => {
     /** gallery init **/
     $("#lightgallery_new").lightGallery(galleryOptions_new)
     $("#lightgallery_old").lightGallery(galleryOptions_old)
 
     /** socket init **/
-    const socket = io.connect()
+    const socket = await io.connect()
 
     socket.emit('user')
 
@@ -61,13 +61,15 @@ $(document).ready(function() {
         fireToast(filename)
     })
 
+    await createQuestionTable(84)
+    $('.tooltipped').tooltip()
 })
 
 /** add new screen to new screens **/
 function addNewScreen(name) {
     const slide =
     `  <a href="${name}">
-          <div class="caption">
+          <div class="caption hoverable z-depth-1 center-align">
             <img src="${name}" />
             <p>${name.slice(1, name.length - 4)}</p>
           </div>
@@ -81,7 +83,7 @@ function addNewScreen(name) {
 function addOldScreen(name) {
     const slide =
         `  <a href="${name}">
-          <div class="caption">
+          <div class="caption hoverable z-depth-1 center-align">
             <img src="${name}" />
             <p>${name.slice(1, name.length - 4)}</p>
           </div>
@@ -91,6 +93,51 @@ function addOldScreen(name) {
     $("#lightgallery_old").lightGallery(galleryOptions_old)
 }
 
+/** fire notification **/ //todo sound
 function fireToast(name) {
     M.toast({html: `Новый скрин ${name.slice(1, name.length - 4)}`})
+}
+
+/** create checkButton **/
+function createCheckButton(number) {
+    return new Promise((resolve, reject) => {
+        const id = number
+        let content = number.toString()
+        if (number < 10)
+            content += '&nbsp '
+        const button = `
+        <a class="waves-effect waves-light btn question_btn grey darken-1" id="question_${number}">${content}</a>
+        `
+        $('#question_card').append(button)
+        resolve(number)
+    })
+}
+
+/** create question table **/
+async function createQuestionTable(count) {
+    for (let i = 1; i <= count; i++) {
+        await createCheckButton(i)
+        $(`#question_${i}`).click(e => {
+            if ($(`#question_${i}`).hasClass('grey darken-1')) {
+                $(`#question_${i}`).toggleClass('grey darken-1')
+                $(`#question_${i}`).toggleClass('cyan lighten-3')
+            }
+            else if ($(`#question_${i}`).hasClass('cyan lighten-3')) {
+                $(`#question_${i}`).toggleClass('cyan lighten-3')
+                $(`#question_${i}`).toggleClass('yellow accent-4')
+            }
+            else if ($(`#question_${i}`).hasClass('yellow accent-4')) {
+                $(`#question_${i}`).toggleClass('yellow accent-4')
+                $(`#question_${i}`).toggleClass('red darken-1')
+            }
+            else if ($(`#question_${i}`).hasClass('red darken-1')) {
+                $(`#question_${i}`).toggleClass('red darken-1')
+                $(`#question_${i}`).toggleClass('green accent-4')
+            }
+            else if ($(`#question_${i}`).hasClass('green accent-4')) {
+                $(`#question_${i}`).toggleClass('green accent-4')
+                $(`#question_${i}`).toggleClass('grey darken-1')
+            }
+        })
+    }
 }
