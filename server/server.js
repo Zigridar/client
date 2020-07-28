df21768a2bc9af7228f76c28f83c89f1015e19c5
zigridar <safonenko.max@yandex.ru>
2020-07-28 21:55:59 +0500

    server safety

--- server/server.js
+++ server/server.js
@@ -27,46 +27,68 @@ app.get('/', (req, res) => {
 
 /** check authorization **/
 app.post('/', (req, res) => {
-    const { login, password } = req.body
-    /** find user **/
-    const user = socketController.users.find(user => user.login === login)
-    /** if user exists and password isn't wrong **/
-    if (serverUtils.validateServerUser(user, password)) {
-        const authToken = serverUtils.generateAuthToken()
-        socketController.authTokens[authToken] = user
-        res.cookie('AuthToken', authToken)
-        res.redirect('/main')
-        console.log(`user ${user.login} has authorized, ${new Date()}`)
+    try {
+        const { login, password } = req.body
+        /** find user **/
+        const user = socketController.users.find(user => user.login === login)
+        /** if user exists and password isn't wrong **/
+        if (serverUtils.validateServerUser(user, password)) {
+            const authToken = serverUtils.generateAuthToken()
+            socketController.authTokens[authToken] = user
+            res.cookie('AuthToken', authToken)
+            res.redirect('/main')
+            console.log(`user ${user.login} has authorized, ${new Date()}`)
+        }
+        else {
+            res.redirect('/')
+            console.log(`authorisation failed with login ${login}, ${new Date()}`)
+        }
     }
-    else {
-        res.redirect('/')
-        console.log(`authorisation failed with login ${login}, ${new Date()}`)
+    catch (e) {
+        console.error(`route error (/), ${new Date()}`)
+        console.error(e)
     }
 })
 
 /** set authToken **/
 app.use((req, res, next) => {
-    const authToken = req.cookies['AuthToken']
-    req.user = socketController.authTokens[authToken]
-    next()
+    try {
+        const authToken = req.cookies['AuthToken']
+        req.user = socketController.authTokens[authToken]
+        next()
+    }
+    catch (e) {
+        console.error(`route error (use), ${new Date()}`)
+        console.error(e)
+    }
 })
 
 /** admin page **/
 app.get('/admin', (req, res) => {
-
-    if (req.user && req.user.adminAccess)
-        res.sendFile(__dirname  + '/user_content/admin.html')
-    else
-        res.redirect('/main')
-
+    try {
+        if (req.user && req.user.adminAccess)
+            res.sendFile(__dirname  + '/user_content/admin.html')
+        else
+            res.redirect('/main')
+    }
+    catch (e) {
+        console.error(`route error (/admin), ${new Date()}`)
+        console.error(e)
+    }
 })
 
 /** main page **/
 app.get('/main', (req, res) => {
-    if (req.user)
-        res.sendFile(__dirname + '/user_content/index.html')
-    else
-        res.redirect('/')
+    try {
+        if (req.user)
+            res.sendFile(__dirname + '/user_content/index.html')
+        else
+            res.redirect('/')
+    }
+    catch (e) {
+        console.error(`route error (/main), ${new Date()}`)
+        console.error(e)
+    }
 })
 
 /** user content **/
